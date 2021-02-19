c0a76695b8c2b3091f35ea85b3fd80f626d23b47
Keylogger-hash <pvvolo2200@gmail.com>
2021-02-19 21:58:09 +0500

    Button

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <new-button></new-button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import NewButton from './components/NewButton.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NewButton
   }
 }
 </script>
---
+++ src/components/NewButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Count: {{count}}</h1>
+    <button type="buttton" @click="clickOnMe">Click on me</button>
+</template>
+
+<script>
+export default{
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    clickOnMe() {
+      this.count += 1
+    }
+  }
+}
+</script>
